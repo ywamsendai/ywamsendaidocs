8ae511bd94b68346d69aaec7e0e1be0605aa95ef
metatek <gary.seddon@gmail.com>
2025-03-22 19:40:46 +0900

    updated domain name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ywamsendaidocs.netlify.app',
+  url: 'https://docs.ywamsendai.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
